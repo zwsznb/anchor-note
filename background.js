4d86640782f78bbcc58dcc37f7f1aab9bcfbe14c
钟文森 <562009449@qq.com>
2022-12-06 19:36:40 +0800

    refactor: 删除不必要代码

--- background.js
+++ background.js
@@ -1,4 +1,3 @@
-const match_url = 'https://linux.vbird.org/linux_basic/centos7/0320bash.php'
 async function getCurrentTab() {
     let queryOptions = { active: true, lastFocusedWindow: true };
     let [tab] = await chrome.tabs.query(queryOptions);
@@ -6,14 +5,13 @@ async function getCurrentTab() {
 }
 
 chrome.action.onClicked.addListener(async(tab) => {
-    if (tab.url.startsWith(match_url)) {
-        await chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            files: ["/scripts/anchor.js"]
-        }, () => {})
-        await chrome.scripting.insertCSS({
-            target: { tabId: tab.id },
-            files: ["/css/common.css"]
-        });
-    }
+    await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ["/scripts/anchor.js"]
+    }, () => {})
+    await chrome.scripting.insertCSS({
+        target: { tabId: tab.id },
+        files: ["/css/common.css"]
+    });
+
 })
