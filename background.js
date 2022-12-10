da05944eb67d93c9d923a167abb444538fa4bb1b
“zwsznb” <562009449@qq.com>
2022-12-10 19:09:22 +0800

    fix: 重复插入脚本问题

--- background.js
+++ background.js
@@ -1,27 +1,44 @@
+let active_url = [];
 chrome.action.onClicked.addListener(async (tab) => {
+    let url = getUrl(tab);
+    if (active_url.includes(url)) {
+        return;
+    } else {
+        chrome.storage.local.get([url]).then(result => {
+            if (!result[url]) {
+                chrome.storage.local.set({
+                    [url]: []
+                }).then(() => {
+                    console.log("init data");
+                });
+            }
+        });
+        await chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            files: ["/scripts/dataSource.js", "/scripts/anchor.js", "/scripts/note.js"]
+        }, () => { })
+        await chrome.scripting.insertCSS({
+            target: { tabId: tab.id },
+            files: ["/css/common.css"]
+        });
+        active_url.push(url);
+    }
+})
+
+function getUrl(tab) {
     let url = '';
     if (tab.url.includes('#')) {
         url = tab.url.split('#')[0];
     } else {
         url = tab.url;
     }
-    chrome.storage.local.get([url]).then(result => {
-        if (!result[url]) {
-            chrome.storage.local.set({
-                [url]: []
-            }).then(() => {
-                console.log("init data");
-            });
+    return url;
+}
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+    for (let i = 0; i < active_url.length; i++) {
+        if (getUrl(tab) === active_url[i]) {
+            active_url.splice(i, 1);
         }
-    });
-
-    await chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ["/scripts/dataSource.js", "/scripts/anchor.js", "/scripts/note.js"]
-    }, () => { })
-    await chrome.scripting.insertCSS({
-        target: { tabId: tab.id },
-        files: ["/css/common.css"]
-    });
-
+    }
 })
